c8b66f94341476048d5175c6f0a164264b78af34
ChristosCM <christoumavropoulos.christos@gmail.com>
2019-11-15 10:35:27 +0000

    protocols excluded

---
+++ Scripts/oldcode.py
@@ -0,0 +1,21 @@
+def excludePorts(data):
+      # for index,port in excSport.iterrows():
+    #     for _,row in data.iterrows():
+    #         if int(port.sport)==int(row.sport):
+    #             if int(row.Label)==0:
+    #                 print(index,row.sport)
+    #                 excSport.loc[index,'normal'] = int(excSport.loc[index,'normal'])+ 1
+    #             else:
+    #                 excSport.loc[index,'abnormal'] += 1
+    #                 excSport.loc[index,'excluded'] = False
+    
+
+    # for index,port in tqdm(excDsport.iterrows(),total = excDsport.shape[0]):
+    #     for _,row in data.iterrows():
+    #         if int(port.dsport)==int(row.dsport):
+    #             if int(row.Label)==0:
+    #                 excDsport.loc[index,'normal'] = int(excDsport.loc[index,'normal'])+ 1
+    #             else:
+    #                 excDsport.loc[index,'abnormal'] += 1
+    #                 excDsport.loc[index,'excluded'] = False
+    return None
